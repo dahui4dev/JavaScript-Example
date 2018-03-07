95509fa2ad163980d5eff2192a049fc28db86cb7
wangjianhui2464 <wangjianhui2464@gmail.com>
2018-03-07 18:15:23 +0800

    test vue

---
+++ testVue/src/components/childLi.vue
@@ -0,0 +1,25 @@
+<template>
+  <li v-if='localItem.isChecked'>{{localItem}}</li>
+</template>
+
+<script>
+  export default {
+    name: "child-li",
+    data() {
+      return {
+        localItem: this.item,
+      }
+    },
+    props: ['item'],
+    created() {
+      console.log('----------create----------', this)
+    },
+    destoryed() {
+      console.log('----------destoryed----------')
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
